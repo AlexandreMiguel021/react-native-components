1e4174a945d5460e7c8f53fe61275a36164d71fa
Alexandre <alexandremigueldev@outlook.com>
2023-02-13 13:52:57 -0300

    refactor: rename form control files

--- components/FormControl/FormControl.component.tsx
+++ components/FormControl/FormControl.component.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import { View } from "react-native";
 import { Text } from "../text";
-import { styles } from "./form-control-styles";
+import { styles } from "./FormControl.styles";
 
 type FormControlRootProps = {
   children: ReactNode;
